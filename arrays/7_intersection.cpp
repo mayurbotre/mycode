113ebc8c36249808b55f12ca7e2ca684addc695a
mayurbotre <mayurbotre95@gmail.com>
2024-05-26 23:54:50 +0530

    removing unused comments

--- arrays/7_intersection.cpp
+++ arrays/7_intersection.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printIntersection(int arr1[], int arr2[], int m, int n)
-{
+void printIntersection(int arr1[], int arr2[], int m, int n){
 	int i = 0, j = 0;
 	while (i < m && j < n) {
 		if (arr1[i] < arr2[j])
 			i++;
 		else if (arr2[j] < arr1[i])
 			j++;
-		else /* if arr1[i] == arr2[j] */
-		{
+		else {
 			cout << arr2[j] << " ";
 			i++;
 			j++;
