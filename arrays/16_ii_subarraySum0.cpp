fe259a177c34fc3c4a630c012dbaeeb8236c8498
mayurbotre <mayurbotre95@gmail.com>
2024-05-28 01:00:55 +0530

    Subarray with 0 sum using Hashing Optimal

---
+++ arrays/16_ii_subarraySum0.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool subArrayExists(int arr[], int N)
+{
+	unordered_set<int> sumSet;
+	int sum = 0;
+	for (int i = 0; i < N; i++) {
+		sum += arr[i];
+
+		if (sum == 0 || sumSet.find(sum) != sumSet.end())
+			return true;
+
+		sumSet.insert(sum);
+	}
+	return false;
+}
+
+int main()
+{
+	int arr[] = {-3, 2, 3, 1, 6};
+	int N = sizeof(arr) / sizeof(arr[0]);
+
+	if (subArrayExists(arr, N))
+		cout << "Found a subarray with 0 sum";
+	else
+		cout << "No Such Sub Array Exists!";
+	return 0;
+}
