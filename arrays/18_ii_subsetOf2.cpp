d06c620a578874c01c388e1e0159a54298247166
mayurbotre <mayurbotre95@gmail.com>
2024-05-29 23:48:05 +0530

    Find whether an array is a subset of another array  Best

---
+++ arrays/18_ii_subsetOf2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isSubset(int arr1[], int m, int arr2[], int n)
+{
+    set<int> hashset;
+    for (int i = 0; i < m; i++) {
+        hashset.insert(arr1[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        if (hashset.find(arr2[i]) == hashset.end())
+            return false;
+    }
+    return true;
+}
+
+int main(){
+    int arr1[] = { 11, 1, 13, 21, 3, 7 };
+    int arr2[] = { 11, 3, 7, 1 };
+    int m = sizeof(arr1) / sizeof(arr1[0]);
+    int n = sizeof(arr2) / sizeof(arr2[0]);
+
+    if (isSubset(arr1, m, arr2, n))
+        cout << "arr2[] is subset of arr1[] "
+             << "\n";
+    else
+        cout << "arr2[] is not a subset of arr1[] "
+             << "\n";
+    return 0;
+}
