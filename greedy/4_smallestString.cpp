68495f11f9fbeca1917fa9ffae37f004d0de4f09
mayurbotre <77835852+mayurbotre@users.noreply.github.com>
2024-06-15 18:26:03 +0000

    Swap all occurrences of two characters to get lexicographically smallest string

---
+++ greedy/4_smallestString.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 26
+
+string smallestStr(string str, int n)
+{
+	int i, j;
+
+	int chk[MAX];
+	for (i = 0; i < MAX; i++) chk[i] = -1;
+
+	for (i = 0; i < n; i++) {
+		if (chk[str[i] - 'a'] == -1)
+			chk[str[i] - 'a'] = i;
+	}
+
+	for (i = 0; i < n; i++) {
+
+		bool flag = false;
+
+		for (j = 0; j < str[i] - 'a'; j++) {
+
+			if (chk[j] > chk[str[i] - 'a']) {
+				flag = true;
+				break;
+			}
+		}
+
+		if (flag) break;
+	}
+
+	if (i < n-1) {
+
+		char ch1 = str[i];
+		char ch2 = char(j + 'a');
+
+		for (i = 0; i < n; i++) {
+
+			if (str[i] == ch1) str[i] = ch2;
+
+			else if (str[i] == ch2) str[i] = ch1;
+		}
+	}
+
+	return str;
+}
+
+int main()
+{
+	string str = "ccad";
+	int n = str.length();
+
+	cout << smallestStr(str, n);
+
+	return 0;
+}
