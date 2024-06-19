478e6e22c5fa198ce0aef97ae4d618227c35d984
mayurbotre <77835852+mayurbotre@users.noreply.github.com>
2024-06-19 18:10:29 +0000

    ARRANGE -Arranging Amplifiers

---
+++ greedy/17_arrange.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        int oneCount=0;
+        for(int i=0;i<n;i++){ 
+            cin>>arr[i];
+            if(arr[i]==1){
+                oneCount++;
+            }
+        }
+        sort(arr, arr+n, greater<int>());
+
+        for(int i=0;i<oneCount;i++) cout<<"1 ";
+
+        if((n-oneCount)==2 and arr[0]==3 and arr[1]==2){
+            cout<<"2 3";
+        } else {
+            for(int i=0;i<(n-oneCount);i++) 
+                cout<<arr[i]<< " ";
+            cout<<endl;
+        }
+    }
+    return 0;
+}
